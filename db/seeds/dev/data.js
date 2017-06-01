2b93652319b14cd82feb51f42562ef0e0a4b322d
Rebecca Holt <rebecca.holt9991@gmail.com>
2017-06-01 10:08:34 -0600

    Delete commented out code

--- db/seeds/dev/data.js
+++ db/seeds/dev/data.js
@@ -1,7 +1,3 @@
-// const path = require('path');
-//
-// const seedFile = require('knex-seed-file');
-
 exports.seed = function(knex, Promise) {
   return knex('items').del()
     .then(() => knex('garage').del())
